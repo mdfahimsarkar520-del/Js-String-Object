84d46759a9e2f1ea4dd0f6c053e0520283025675
MD Fahim Sarkar <mdfahimsarkar520@gamil.com>
2026-05-15 11:31:44 +0800

    first commit

---
+++ string.js
@@ -0,0 +1,20 @@
+const country ='Bangladesh';
+console.log(country)
+const division ="Rangpur";
+const district ='kurigram';
+// const thana = new String('Rajarhat');
+
+// console.log(thana);
+
+const numbers =[54,52,25,215,];
+// console.log(numbers.length);
+// console.log(numbers[1])
+
+ //const capital ='Dhaka';
+// console.log(capital.length)
+//String is immutable --> not changeable 
+ const capital ='Dhaka';
+
+ console.log(capital);
+ capital[0]='f';
+ console.log(capital);
